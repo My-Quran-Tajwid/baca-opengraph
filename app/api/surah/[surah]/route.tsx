2967735aecbc050f61aa5a4efa1d8a8c9812cbef
iqfareez <60868965+iqfareez@users.noreply.github.com>
2025-01-23 22:52:37 +0800

    :pencil2: Change api route and query path
    Added 5 allternate designs
    Updated README

---
+++ app/api/surah/[surah]/route.tsx
@@ -0,0 +1,98 @@
+import { url } from 'inspector';
+import { ImageResponse } from 'next/og';
+import { NextRequest } from 'next/server';
+
+export const runtime = "edge"
+
+async function loadSurahFont() {
+    const response = await fetch("https://github.com/My-Quran-Tajwid/baca-opengraph/raw/refs/heads/main/public/fonts/QCF4_Surah-Regular.ttf")
+    if (response.status == 200) {
+        return await response.arrayBuffer()
+    }
+
+    throw new Error('failed to load font data')
+
+}
+export async function GET(request: NextRequest, { params }: { params: Promise<{ surah: string }> }) {
+    const searchParams = request.nextUrl.searchParams;
+    const surahNumber = (await params).surah;
+    const alternateQuery = parseInt(searchParams.get('alt') ?? '0', 10);
+
+    const symbolSet = [
+        '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-',
+        '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':',
+        ';', '<', '=', '>', '?', '@',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        '[', '\\', ']', '^', '_', '`',
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        '{', '|', '}', '~', '¡', '¢', '£', '¤', '¥', '¦', '§', '¨', '©', 'ª',
+        '«', '¬', '®', '¯', '°', '±', '²', '³', '´', 'µ'
+    ];
+    const surahIndex = parseInt(surahNumber) - 1;
+    const surahSymbol = symbolSet[surahIndex];
+
+    const alternateSet = [
+        {
+            // Vercel's Gradient (OG Playground)
+            backgroundImage: 'linear-gradient(to bottom, #dbf4ff, #fff1f1)',
+            color: 'black',
+        },
+        {
+            backgroundImage: "url('https://res.cloudinary.com/iqfareez-cloud/image/upload/v1737603518/metatags/BacaOG/3_wrwxcb.png')",
+            color: 'black',
+        },
+        {
+            backgroundImage: "url('https://res.cloudinary.com/iqfareez-cloud/image/upload/v1737603517/metatags/BacaOG/2_sxkgx8.png')",
+            color: 'white',
+        },
+        {
+            backgroundImage: "url('https://res.cloudinary.com/iqfareez-cloud/image/upload/v1737603518/metatags/BacaOG/1_llo1h5.png')",
+            color: 'black',
+        },
+        {
+            backgroundImage: "url('https://res.cloudinary.com/iqfareez-cloud/image/upload/v1737603518/metatags/BacaOG/4_vzbucf.png')",
+            color: 'antiquewhite',
+            dropShadow: true,
+        },
+        {
+            backgroundImage: "url('https://res.cloudinary.com/iqfareez-cloud/image/upload/v1737603518/metatags/BacaOG/5_wfmdky.png')",
+            color: 'black',
+        }
+
+    ]
+
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    display: 'flex',
+                    fontSize: 180,
+                    color: alternateSet[alternateQuery].color,
+                    backgroundImage: alternateSet[alternateQuery].backgroundImage,
+                    width: '100%',
+                    height: '100%',
+                    padding: '50px 200px',
+                    textAlign: 'center',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textShadow: alternateSet[alternateQuery].dropShadow ? '4px 4px 16px rgba(0, 0, 0, 0.5)' : 'none',
+                }}
+            >
+                {surahSymbol}
+            </div>
+        ),
+        {
+            width: 1200,
+            height: 630,
+            fonts: [
+                {
+                    name: "QCF_Surah",
+                    data: await loadSurahFont(),
+                },
+
+            ]
+        },
+    );
+}
