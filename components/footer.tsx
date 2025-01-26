9f32bc2575bc4df7f145cbd8ef1accf0c8366489
iqfareez <60868965+iqfareez@users.noreply.github.com>
2025-01-26 08:09:44 +0800

    :pencil2: (Footer) Add link to parent project

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { Figtree } from "next/font/google";
+import Link from "next/link";
 
 const figtree = Figtree({
     subsets: ['latin'],
@@ -10,9 +11,11 @@ const figtree = Figtree({
 
 export default function Footer() {
     return (
-        <footer className={`flex items-center justify-center ${figtree.className} my-8`}>
-            <Image src="/images/baca-logo.png" alt="QUranTajwid Logo" width={36} height={36} />
-            <span className="ml-2 text-lg text-rose-700 dark:text-rose-400 font-bold">QuranTajwid</span>
-        </footer >
+        <Link href="https://qurantajwid.my">
+            <footer className={`flex items-center justify-center ${figtree.className} my-8`}>
+                <Image src="/images/baca-logo.png" alt="QuranTajwid Logo" width={36} height={36} />
+                <span className="ml-2 text-lg text-rose-700 dark:text-rose-400 font-bold">QuranTajwid</span>
+            </footer >
+        </Link>
     )
 }
